5011e8b4a72c282142812a5f453ca3bd4a298157
kaleemullah12 <37169000+kaleemullah12@users.noreply.github.com>
2018-10-08 23:44:34 -0700

    ash

---
+++ SchoolSystem/SchoolSystem/Controllers/HomesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolSystem.Controllers
+{
+    public class HomesController : Controller
+    {
+        // GET: Homes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AdminLogIN()
+        {
+            return View();
+        }
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
